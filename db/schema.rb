3edaa1664cf1b734c01d3c49fd4ba8794eb6907c
Sokhoeun Lim <sknlim@gmail.com>
2015-02-11 12:55:59 -0800

    Change email to name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211191630) do
+ActiveRecord::Schema.define(version: 20150211205132) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150211191630) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
